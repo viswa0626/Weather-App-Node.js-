459406cc3bdca568952e3f8bbcd3f0c5ae8ffa56
viswa <18b01a02a2@svecw.edu.in>
2023-07-08 11:42:12 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -1,62 +1,67 @@
 const path = require('path')
-const express = require('express');
-// console.log(__dirname);
-// console.log(__filename);
+const express = require('express')
+const hbs = require('hbs')
 
+const app = express()
 
-const app=express();
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-//Define paths for Express config
-const publicDirectoryPath = path.join(__dirname,'../public');
-const viewPath = path.join(__dirname,'../templates/views')
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
-//Setup wgv
-app.get('',(req,res)=>{
-    res.render('index',{
-        title: 'Weather App',
-        
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Andrew Mead'
     })
 })
-app.get('/about',(req,res)=>{
-    res.render('about',{
-        title:'About Page',
-        name:'Viswa'
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
     })
 })
 
-
-app.get('/help',(req,res)=>{
-    res.render('help',{
-        title:'Help',
-        message:'How can i Help you'
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Andrew Mead'
     })
-
 })
 
-// app.get('',(req,res)=>{
-//     res.send('Hello Express');
-// })
-
-
-// app.get('/help',(req,res)=>{
-//     res.send("<h1><b>Help Page</b></h1>");
-
-// })
-
-// app.get('/about',(req,res)=>{
-//     res.send([
-//         {
-//             name:"Andrew"
-//         },{
-//             age:30
-//         }
-//     ]);
-// })
-
-app.get('/weather',(req,res)=>{
-    res.send("Weather Page")
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
 })
 
-app.listen(3000,()=>{
-    console.log("Server is running up");
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
 })
